2dbf2316b0449f1bf822789ea87d09ec5dc489ea
Dani-Martin3101 <60471910+Dani-Martin3101@users.noreply.github.com>
2022-02-10 12:15:36 +0100

    Update setupPrestashop.sh

--- setupPrestashop.sh
+++ setupPrestashop.sh
@@ -5,7 +5,6 @@ a2enmod rewrite
 systemctl restart apache2
 chown -R pi:www-data /var/www/html/
 chmod -R 770 /var/www/html/
-chown -R www-data:www-data /var/www/html/prestashop
 
 apt install -y php libapache2-mod-php
 apt install -y zip unzip
@@ -14,6 +13,7 @@ apt install -y php-zip php-simplexml php-curl php-gd php-mysql php-intl php-mbst
 service apache2 restart
 
 mkdir /var/www/html/prestashop
+chown -R www-data:www-data /var/www/html/prestashop
 cd /var/www/html/prestashop
 wget https://github.com/PrestaShop/PrestaShop/releases/download/1.7.8.3/prestashop_1.7.8.3.zip
 unzip prestashop_1.7.8.3.zip
